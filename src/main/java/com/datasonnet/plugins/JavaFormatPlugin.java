5e2d4a717f2c80fa1e9a200ed0619089c27ac11e
Eugene Berman <eugene.berman@modusbox.com>
2020-01-06 09:23:54 -0700

    Date format support

--- src/main/java/com/datasonnet/plugins/JavaFormatPlugin.java
+++ src/main/java/com/datasonnet/plugins/JavaFormatPlugin.java
@@ -12,6 +12,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ujson.Value;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -19,11 +21,16 @@ import java.util.Map;
 
 public class JavaFormatPlugin implements DataFormatPlugin {
     public static String OUTPUT_CLASS = "OutputClass";
+    public static String DATE_FORMAT = "DateFormat";
+
+    public static String DEFAULT_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
     public JavaFormatPlugin() { }
 
     public Value read(Object input, Map<String, Object> params) throws PluginException {
         ObjectMapper mapper = new ObjectMapper();
+        DateFormat df = new SimpleDateFormat(params.containsKey(DATE_FORMAT) ? (String)params.get(DATE_FORMAT) : DEFAULT_DATE_FORMAT);
+        mapper.setDateFormat(df);
         try {
             String jsonStr = mapper.writeValueAsString(input);
             return UjsonUtil.jsonObjectValueOf(jsonStr);
@@ -36,6 +43,8 @@ public class JavaFormatPlugin implements DataFormatPlugin {
         try {
             String jsonString = UjsonUtil.jsonObjectValueTo(input);
             ObjectMapper mapper = new ObjectMapper();
+            DateFormat df = new SimpleDateFormat(params.containsKey(DATE_FORMAT) ? (String)params.get(DATE_FORMAT) : DEFAULT_DATE_FORMAT);
+            mapper.setDateFormat(df);
             JsonNode node = mapper.readTree(jsonString);
 
             JavaType valueType = null;
@@ -68,15 +77,16 @@ public class JavaFormatPlugin implements DataFormatPlugin {
 
     @Override
     public Map<String, String> getReadParameters() {
-//        Map<String, String> readParams = new HashMap<>();
-//        return readParams;
-        return Collections.emptyMap();
+        Map<String, String> readParams = new HashMap<>();
+        readParams.put(DATE_FORMAT, "Controls the date format for serializing/deserializing");
+        return readParams;
     }
 
     @Override
     public Map<String, String> getWriteParameters() {
         Map<String, String> writeParams = new HashMap<>();
         writeParams.put(OUTPUT_CLASS, "Fully qualified class name of the output");
+        writeParams.put(DATE_FORMAT, "Controls the date format for serializing/deserializing");
         return writeParams;
     }
 
