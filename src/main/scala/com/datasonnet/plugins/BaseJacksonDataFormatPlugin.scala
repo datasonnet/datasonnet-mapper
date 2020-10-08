6c4f335690e8a64155b05764e90725a3c52fcbf1
Jose Montoya <jam01@protonmail.com>
2020-10-07 20:13:07 -0500

    fixes null handling

--- src/main/scala/com/datasonnet/plugins/BaseJacksonDataFormatPlugin.scala
+++ src/main/scala/com/datasonnet/plugins/BaseJacksonDataFormatPlugin.scala
@@ -18,7 +18,7 @@ package com.datasonnet.plugins
 
 import com.datasonnet.spi.AbstractDataFormatPlugin
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.node.{ArrayNode, BooleanNode, NumericNode, ObjectNode, TextNode}
+import com.fasterxml.jackson.databind.node.{ArrayNode, BooleanNode, NullNode, NumericNode, ObjectNode, TextNode}
 import ujson.Value
 
 import scala.jdk.CollectionConverters.IteratorHasAsScala
@@ -30,6 +30,7 @@ abstract class BaseJacksonDataFormatPlugin extends AbstractDataFormatPlugin {
     case s: TextNode => ujson.Str(s.textValue())
     case o: ObjectNode => ujson.Obj.from(o.fields.asScala.map(entry => (entry.getKey, ujsonFrom(entry.getValue))))
     case a: ArrayNode => ujson.Arr.from(a.elements.asScala.map(ujsonFrom))
+    case _: NullNode => ujson.Null
     case _ => null
   }
 }
