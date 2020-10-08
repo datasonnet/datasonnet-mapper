89f1bdb54c89c949b07c1f6468e1784f31ec1a12
Jake <jhughes@ms3-inc.com>
2020-10-08 09:23:08 -0400

    Removed the unused functions from util

--- src/test/java/com/datasonnet/UtilLibraryTest.java
+++ src/test/java/com/datasonnet/UtilLibraryTest.java
@@ -28,54 +28,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UtilLibraryTest {
 
     @Test
-    void testDuplicates() throws Exception {
-        String jsonData = TestResourceReader.readFileAsString("utilLibDuplicatesTest.json");
-
-        Mapper mapper = new Mapper("ds.util.duplicates(payload.primitive)");
-        String mappedJson = mapper.transform(new DefaultDocument<String>(jsonData, MediaTypes.APPLICATION_JSON), Collections.emptyMap(), MediaTypes.APPLICATION_JSON).getContent();
-        assertEquals(mappedJson, "[\"hello\",\"world\"]");
-
-        mapper = new Mapper("ds.util.duplicates(payload.complex, function(x) x.language.name)");
-        mappedJson = mapper.transform(new DefaultDocument<String>(jsonData, MediaTypes.APPLICATION_JSON), Collections.emptyMap(), MediaTypes.APPLICATION_JSON).getContent();
-        assertEquals(mappedJson, "[{\"language\":{\"name\":\"Java8\",\"version\":\"1.8.0\"}}]");
-
-        mapper = new Mapper("ds.util.duplicates(payload.moreComplex, function(x) std.substr(x.language.version, 0, 3))");
-        mappedJson = mapper.transform(new DefaultDocument<String>(jsonData, MediaTypes.APPLICATION_JSON), Collections.emptyMap(), MediaTypes.APPLICATION_JSON).getContent();
-        assertEquals(mappedJson, "[{\"language\":{\"name\":\"Java1.8\",\"version\":\"1.8_152\"}}]");
-    }
-
-    @Test
-    void testRemoveFields() throws Exception {
-        String jsonData = TestResourceReader.readFileAsString("utilLibRemoveFieldsTest.json");
-        testDS("utilLibRemoveFieldsTest.ds", jsonData);
-    }
-
-    @Test
-    void testReverse() throws Exception {
-        String jsonData = "[\"a\",\"b\",\"c\",\"d\"]";
-        Mapper mapper = new Mapper("ds.reverse(payload)");
-        String mappedJson = mapper.transform(new DefaultDocument<String>(jsonData, MediaTypes.APPLICATION_JSON), Collections.emptyMap(), MediaTypes.APPLICATION_JSON).getContent();
-
-        assertEquals(mappedJson, "[\"d\",\"c\",\"b\",\"a\"]");
-    }
-
-    @Test
-    void testGroupBy() throws Exception {
-        String jsonData = TestResourceReader.readFileAsString("utilLibGroupByTest.json");
-        testDS("utilLibGroupByTest.ds", jsonData);
-    }
-
-    @Test
-    void testRound() throws Exception {
-        testDS("utilLibRoundTest.ds", "{}");
-    }
-
-    @Test
-    void testCounts() throws Exception {
-        testDS("utilLibCountsTest.ds", "{}");
-    }
-
-    @Test
     void testMapToObject() throws Exception {
         testDS("utilLibMapToObjectTest.ds", "{}");
     }
