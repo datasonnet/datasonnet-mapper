5b8efda97180c616227a6cb4d728bdcfcf619192
Eugene Berman <eugene.berman@modusbox.com>
2020-10-19 10:06:14 -0700

    Added TimeZone support to Java de/serializer

--- src/main/java/com/datasonnet/plugins/JavaFormatPlugin.java
+++ src/main/java/com/datasonnet/plugins/JavaFormatPlugin.java
@@ -21,13 +21,16 @@ import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
 public class JavaFormatPlugin implements DataFormatPlugin<Object> {
     public static String OUTPUT_CLASS = "OutputClass";
     public static String DATE_FORMAT = "DateFormat";
+    public static String TIMEZONE = "TimeZone";
     public static String FAIL_ON_EMPTY_BEANS = "FailOnEmptyBeans";
 
     public static String DEFAULT_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    public static String DEFAULT_TIMEZONE = "UTC";
 
     public JavaFormatPlugin() { }
 
@@ -38,6 +41,7 @@ public class JavaFormatPlugin implements DataFormatPlugin<Object> {
         module.addSerializer(JAXBElement.class, new JAXBElementSerializer());
         mapper.registerModule(module);
         DateFormat df = new SimpleDateFormat(params.containsKey(DATE_FORMAT) ? (String)params.get(DATE_FORMAT) : DEFAULT_DATE_FORMAT);
+        df.setTimeZone(TimeZone.getTimeZone(params.containsKey(TIMEZONE) ? (String) params.get(TIMEZONE) : DEFAULT_TIMEZONE));
         mapper.setDateFormat(df);
 
         if (params.containsKey(FAIL_ON_EMPTY_BEANS)) {
@@ -60,6 +64,7 @@ public class JavaFormatPlugin implements DataFormatPlugin<Object> {
             ObjectMapper mapper = new ObjectMapper();
             mapper.addMixIn(JAXBElement.class, JAXBElementMixIn.class);
             DateFormat df = new SimpleDateFormat(params.containsKey(DATE_FORMAT) ? (String) params.get(DATE_FORMAT) : DEFAULT_DATE_FORMAT);
+            df.setTimeZone(TimeZone.getTimeZone(params.containsKey(TIMEZONE) ? (String) params.get(TIMEZONE) : DEFAULT_TIMEZONE));
             mapper.setDateFormat(df);
 
             final JsonNode node = mapper.readTree(jsonString);
@@ -97,6 +102,7 @@ public class JavaFormatPlugin implements DataFormatPlugin<Object> {
     public Map<String, String> getReadParameters() {
         Map<String, String> readParams = new HashMap<>();
         readParams.put(DATE_FORMAT, "Controls the date format for serializing/deserializing");
+        readParams.put(TIMEZONE, "Controls the date timezone for serializing/deserializing");
         readParams.put(FAIL_ON_EMPTY_BEANS, "Controls whether exception is thrown if no serializer is found for a type");
         return readParams;
     }
@@ -106,6 +112,7 @@ public class JavaFormatPlugin implements DataFormatPlugin<Object> {
         Map<String, String> writeParams = new HashMap<>();
         writeParams.put(OUTPUT_CLASS, "Fully qualified class name of the output");
         writeParams.put(DATE_FORMAT, "Controls the date format for serializing/deserializing");
+        writeParams.put(TIMEZONE, "Controls the date timezone for serializing/deserializing");
         return writeParams;
     }
 
