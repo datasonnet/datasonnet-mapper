a211b03406b4ca1e94bc800d7b82b882c3eded15
Jose Montoya <jam01@protonmail.com>
2021-02-10 10:33:38 -0600

    fix license year

--- src/main/java/com/datasonnet/plugins/DefaultJavaFormatPlugin.java
+++ src/main/java/com/datasonnet/plugins/DefaultJavaFormatPlugin.java
@@ -1,7 +1,7 @@
 package com.datasonnet.plugins;
 
 /*-
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@ package com.datasonnet.plugins;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import com.datasonnet.RecentsMap;
 import com.datasonnet.document.DefaultDocument;
 import com.datasonnet.document.Document;
