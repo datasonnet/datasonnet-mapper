5d59ffc96a151039a475290e37bb05a32060c0af
Eugene Berman <eugene.berman@modusbox.com>
2022-11-18 16:23:36 -0700

    license headers updated

--- src/main/scala/com/datasonnet/spi/Library.scala
+++ src/main/scala/com/datasonnet/spi/Library.scala
@@ -1,7 +1,7 @@
 package com.datasonnet.spi
 
 /*-
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@ package com.datasonnet.spi
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import com.datasonnet.Mapper
 import com.datasonnet.header.Header
 import fastparse.Parsed
--- src/main/scala/com/datasonnet/wrap/NoFileEvaluator.scala
+++ src/main/scala/com/datasonnet/wrap/NoFileEvaluator.scala
@@ -1,7 +1,7 @@
 package com.datasonnet.wrap
 
 /*-
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@ package com.datasonnet.wrap
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import com.datasonnet.jsonnet.{Evaluator, Expr, Path}
 
 class NoFileEvaluator(jsonnet: String,
