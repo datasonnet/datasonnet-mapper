41c0256a13d14ac9c7f4cf57ac87c9daf6d67f5e
Eugene Berman <eugene.berman@portx.io>
2023-05-26 07:07:13 -0700

    License format

--- src/main/java/com/datasonnet/document/DefaultDocument.java
+++ src/main/java/com/datasonnet/document/DefaultDocument.java
@@ -1,7 +1,7 @@
 package com.datasonnet.document;
 
 /*-
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@ package com.datasonnet.document;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /***
  * The DefaultDocument class
  *
